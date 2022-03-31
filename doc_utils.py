55a120f4ac717241fde4d6ac27ca1f31a22bb38e
Chieh-Hung-Cheng <james110012@gmail.com>
2022-03-31 15:13:11 +0800

    testing labeling

--- doc_utils.py
+++ doc_utils.py
@@ -1,6 +1,7 @@
 import os
 import csv
 import pandas
+from datetime import datetime
 import pandas as pd
 
 base_pth = os.path.dirname(__file__)
@@ -19,68 +20,53 @@ def csv_to_df(doc_path):
     return df
 
 
-def get_news():
-    postfixes = ['2019', '2020', '2021']
-    # if typ == 'lst':
+def get_infos(source='news', years=('2021', '2020', '2019'), ret_header=True):
+    years = sorted(years)
     ret_lst = []
-    for postfix in postfixes:
-        ret_lst += csv_to_list(os.path.join(raw_pth, 'news_{}.csv'.format(postfix)))
-    return ret_lst
-    '''elif typ == 'df':
-        ret_df = pandas.DataFrame()
-        for postfix in postfixes:
-            pandas.concat([ret_df, csv_to_df(os.path.join(raw_pth, 'news_{}.csv'.format(postfix)))])
-        return ret_df'''
+    ret_header = []
+    header_exist = False
+    for year in years:
+        if not header_exist:
+            ret_header = csv_to_list(os.path.join(raw_pth, '{}_{}.csv'.format(source, year)))[0:1]
+            header_exist = True
+        ret_lst += csv_to_list(os.path.join(raw_pth, '{}_{}.csv'.format(source, year)))[1:]
+    if ret_header:
+        return ret_header, ret_lst
+    else:
+        return ret_lst
 
 
-def get_forums():
-    postfixes = ['2019-2020', '2021']
+def get_stocks(mkt_typ='0', years=('2021', '2020', '2019'), ret_header=True):
+    # Reorder from old to new
+    years = sorted(years)
     ret_lst = []
-    for postfix in postfixes:
-        ret_lst += csv_to_list(os.path.join(raw_pth, 'forum_{}.csv'.format(postfix)))
-    return ret_lst
-
-
-def get_bbs():
-    postfixes = ['2019-2020', '2021']
-    ret_lst = []
-    for postfix in postfixes:
-        ret_lst += csv_to_list(os.path.join(raw_pth, 'bbs_{}.csv'.format(postfix)))
-    return ret_lst
-
-
-def get_stock_data():
-    '''
-    xls = pd.ExcelFile(os.path.join(raw_pth, 'stock_data_2019-2021.xlsx'))
-    markets = ['上市']  # , '上櫃']
-    years = ['2019', '2020', '2021']
-    df_lst = []
-    for market in markets:
-        for year in years:
-            df_lst.append(pd.read_excel(xls, '{}{}'.format(market,year)))
-        return pd.concat(df_lst, axis=0)
-
-
-    years = ['2019'] #, '2020', '2021']
-    sheetname = '{}{}'.format(markets[0], years[0])
-
-    dfs_lst = []
-    for market in markets:
-        df_lst = []
-        for year in years:
-            
-            df_lst.append(pd.read_excel(os.path.join(raw_pth, 'stock_data_2019-2021.xlsx'), sheet_name=sheetname))
-        dfs_lst.append(pd.concat(df_lst, axis=0))
-
-    df = pd.read_excel(os.path.join(raw_pth, 'stock_data_2019-2021.xlsx'), sheet_name=sheetname)
-    return df'''
+    ret_header = []
+    header_exist = False
+    for year in years:
+        tmp = csv_to_list(os.path.join(raw_pth, 'stock_{}_{}.csv'.format(year, mkt_typ)))
+        header = tmp[0:1]
+        tmp = tmp[1:]
+        tmp.reverse()
+        if not header_exist:
+            ret_header = header
+            # ret_lst += header
+            header_exist = True
+        ret_lst += tmp
+    if ret_header:
+        return ret_header, ret_lst
+    else:
+        return ret_lst
 
 
 def doc_test():
-    news_lst = get_news()
-    forum_lst = get_forums()
-    bbs_lst = get_bbs()
-    stock_lst = get_stock_data()
+    info_header, info = get_infos(years=['2021'], source='news')
+    stock_header, stock = get_stocks(years=['2021'])
+    y  = ['2020/1/31', '2020/1/26', '2020/12/3', '2020/2/1','2015/5/25']
+    ans = []
+    for i in y:
+        ans.append(datetime.strptime(i, '%Y/%m/%d'))
+    ans.sort()
+    print(ans[1]-ans[0])
     pass
 
 
--- labeling.py
+++ labeling.py
@@ -1,13 +1,59 @@
-import doc_utils
+import time
 
-def gen_label():
+import doc_utils
+import datetime
+from tqdm import tqdm
+
+
+def gen_stock_trend(stock_name, stocks_lst, tgt_dict, width=5):
+    date_price_lst = []
+    # Find all entries for that stock
+    for stock in stocks_lst:
+        if stock[0] == stock_name:
+            date_price_lst.append(stock)
+    # Generate trends using terminate price
+    up_lst = []
+    down_lst = []
+    for i in range(len(date_price_lst) - width):
+        up = True
+        down = True
+        for j in range(width):
+            if j == 0:
+                prev = date_price_lst[i + j][5]
+            else:
+                now = date_price_lst[i + j][5]
+                if now > prev:
+                    down = False
+                elif now < prev:
+                    up = False
+                else:
+                    up = False
+                    down = False
+                prev = now
+        # Append passing date to list
+        if up:
+            up_lst.append(date_price_lst[i][1])
+        elif down:
+            down_lst.append(date_price_lst[i][1])
+    tgt_dict[stock_name]= {'UPs': up_lst, 'DOWNs': down_lst}
+
+
+def gen_trends_for_stocks(stocks=('0050 元大台灣50', '2330 台積電'), gen_all_stocks=False, years=('2021', '2020', '2019')):
+    stock_header, stocks_lst = doc_utils.get_stocks(mkt_typ='0', years=years)
+    if gen_all_stocks:
+        stocks = set(i[0] for i in stocks_lst)
+    # generate trend for specified stocks
+    tgt_dict = {}
+    for stock in tqdm(stocks):
+        gen_stock_trend(stock, stocks_lst, tgt_dict)
+    return tgt_dict
+
+
+def labeling_test():
+    ret_dict = gen_trends_for_stocks(gen_all_stocks=True)
+    # gen_stock_trend('0050 元大台灣50', stocks_lst, tgtdict)
     pass
 
 
-def labling_test():
-    stock_df_lst = doc_utils.get_stock_data()
-    label_list = gen_label()
-
-
 if __name__ == '__main__':
-    labling_test()
+    labeling_test()
